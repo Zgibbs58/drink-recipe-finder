5b40d87ec48e19ef872045c536131f592bcfb2f6
skybreakbb <willistaylora@gmail.com>
2023-05-25 20:38:53 -0500

    added results.html, added results.js with fetch request function for all 3 requests

---
+++ assets/js/results.js
@@ -0,0 +1,36 @@
+// function to fetch based on DRINK NAME
+function getDrinkName(inp) {
+    var reqName = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inp}`;
+    fetch(reqName)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+  }
+
+  // function to fetch based on INGREDIENT
+  function getIngrName(inp) {
+    var reqName = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i${inp}`;
+    fetch(reqName)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+  }
+
+  // function to fetch RANDOM
+  function getRandom() {
+    var reqName = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+    fetch(reqName)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+  }
+
