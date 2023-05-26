04e32ca16b3e7b537b93384f890b1c04bd8752a9
skybreakbb <willistaylora@gmail.com>
2023-05-25 21:49:54 -0500

    added function to populate page with results from getIngrName function, added styling to index.html body, commented js

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # drink-recipe-finder
 Find a drink
+
+on return data we need:
+- strDrink (the drink name)
+- strImageSource (picture)
+- strIngredient1-15 (ingredients)
+- strInstructions (instructions in english)
+- strMeasure1-15 (measurements)
+- strVideo NOT ALWAYS APPLICABLE
+
+on INGREDIENT SEARCH return data we need:
+- strDrink
+- strDrinkThumb
--- assets/js/results.js
+++ assets/js/results.js
@@ -15,22 +15,43 @@ function getDrinkName(inp) {
     var reqName = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inp}`;
     fetch(reqName)
     .then(function (response) {
+        JSON.stringify(response);
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-    })
-  }
+    //   console.log(data);
+    popList(data);
+})
+}
 
-  // function to fetch RANDOM
-  function getRandom() {
+// function to populate list created by getIngrName()
+function popList(data) {
+    console.log(data);
+    var ulEl = document.querySelector("#popList");
+    for (let i = 0; i < data.drinks.length; i++) {
+        var liEl = document.createElement("div");
+        var txtEl = document.createElement("p");
+        liEl.setAttribute("class", "box has-text-centered m-5");
+        txtEl.setAttribute("class", "content is-small");
+        liEl.innerHTML = `<img width="128" height="128" src=${data.drinks[i].strDrinkThumb}>`;
+        txtEl.innerHTML = `${data.drinks[i].strDrink}`;
+        ulEl.append(liEl);
+        liEl.append(txtEl);
+    }
+}
+
+// function to fetch RANDOM
+function getRandom() {
     var reqName = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
     fetch(reqName)
     .then(function (response) {
-      return response.json();
+        return response.json();
     })
     .then(function (data) {
-      console.log(data);
+        console.log(data);
     })
-  }
+}
 
+
+// test function run
+getRandom();
